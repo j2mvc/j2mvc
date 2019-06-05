65f6187254f1f930823d24cb0c40f25d5292deec
杨朔 <wwwdev@qq.com>
2019-06-05 22:37:37 +0800

    发布设置，取消发布doc，注释不完整

---
+++ j2mvc-web/src/main/java/com/j2mvc/web/framework/App.java
@@ -0,0 +1,13 @@
+package com.j2mvc.web.framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
